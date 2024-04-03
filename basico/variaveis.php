c10c429e71ee523aee4deb5f0692a90daba67305
Fabio Vige <fabiovige@gmail.com>
2024-04-03 17:42:01 -0300

    variáveis

---
+++ basico/variaveis.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "fabio";
+$idade = 48;
+$valor = 5.6;
+
+define("PI", 3.14159);
+
+echo "Nome: $nome<br>";
+echo "Idade: $idade<br>";
+echo "Valor: $valor<br>";
+echo "PI: " . PI . "<br>";
